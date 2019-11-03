8d1cbdb7bb0b6c0aa8150b6daf3ef0a081b1756e
ErdnaHog <49737083+ErdnaHog@users.noreply.github.com>
2019-11-04 02:05:18 +0800

    Week 4 Flask
    Finished week 4 Flask practical

---
+++ Week 4/Flask/simpleProject.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, redirect, request, url_for
+from Forms import CreateUserForm
+import shelve, User
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/contactUs")
+def contact_us():
+    return render_template("contactUs.html")
+
+@app.route("/createUser", methods=["GET", "POST"])
+def createUser():
+    createUserForm = CreateUserForm(request.form)
+    if request.method == "POST" and createUserForm.validate():        
+        db = shelve.open("storage.db", "c")
+        try:
+            usersDict = db["Users"]
+        except:
+            usersDict = {}
+            print("Error in retrieving Users from storage.db")
+        #? using class to create user instance
+        user_instance = User.User(createUserForm.firstName.data, createUserForm.lastName.data, createUserForm.membership.data, createUserForm.gender.data, createUserForm.remarks.data)
+        usersDict[user.get_userID()] = user_instance
+        db["Users"] = usersDict        
+        db.close()
+
+        return redirect(url_for("retrieveUsers"))
+    else:
+        print("Hi")
+    return render_template("createUser.html", form=createUserForm)
+
+@app.route("/retrieveUsers", methods=["GET", "POST"])
+def retrieveUsers():
+    usersDict = {}
+    db = shelve.open("storage.db", "r")
+    usersDict = db["Users"]
+    db.close()
+
+    usersList = []
+    for value in usersDict.values():
+        usersList.append(value)
+
+    return render_template("retrieveUsers.html", usersList=usersList, count=len(usersList))
+
+if __name__ == "__main__":
+    app.run(debug=True)
